7a5119bfaa9ccb531bbe6804e19b313763bf2660
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-05-09 16:11:56 +0800

    update remove metadata

--- file/metadata_editor.py
+++ file/metadata_editor.py
@@ -9,12 +9,37 @@ class MetadataEditor:
         cmd += [file]
         subprocess.run(cmd)
 
-    def delete_metadata(self, file, metadata_keys):
+    def delete_metadata(self, file, metadata_keys=None):
         """Deletes metadata from a file using exiftool."""
+        if metadata_keys == None:
+            metadata_keys = [
+                "GPS Latitude Ref",
+                "GPS Longitude Ref",
+                "GPS Altitude Ref",
+                "GPS Speed Ref",
+                "GPS Speed",
+                "GPS Img Direction Ref",
+                "GPS Img Direction",
+                "GPS Dest Bearing Ref",
+                "GPS Dest Bearing",
+                "GPS Date Stamp",
+                "GPS Horizontal Positioning",
+                "GPS Altitude",
+                "GPS Latitude",
+                "GPS Longitude",
+                "GPS Position",
+                "Lens Id",
+                # "Camera Model Name",
+                "Host Computer",
+                "Software"
+            ]
+
         cmd = ["exiftool"]
         for k in metadata_keys:
-            cmd += [f"-{k}="]
+            key = k.replace(" ", "")
+            cmd += [f"-{key}="]
         cmd += [file]
+        print(cmd)
         subprocess.run(cmd)
 
     def get_metadata(self, file):
@@ -37,4 +62,14 @@ if __name__ == '__main__':
     file = sys.argv[1]
     metaclass = MetadataEditor()
     meta = metaclass.get_metadata(file=file)
+    print("GET METADATA")
     print(meta)
+
+    
+    print("\n\nDELETE METADATA")
+    deleted_meta = metaclass.delete_metadata(file=file)
+
+    meta = metaclass.get_metadata(file=file)
+    print("\n\nCHECK METADATA")
+    print(meta)
+    
