fb275f723a5769e84525b64872eb30e60158dc03
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2024-06-18 14:53:18 +0800

    update youtube downloader

--- restapi/youtube_downloader.py
+++ restapi/youtube_downloader.py
@@ -1,32 +1,62 @@
 import os
 import sys
 import pytube
-"""
-pip install pytube
-"""
-def download_video(video_url):
-    try:
-        youtube = pytube.YouTube(video_url)
-        video_stream = youtube.streams.get_highest_resolution()
-        video_stream.download()
+import argparse
 
-        original_filename = video_stream.default_filename
-        print(f"Downloading {original_filename}")
-        os.rename(original_filename, original_filename.replace(" ", "-"))
+class YouTubeDownloader:
+    def __init__(self):
+        self.setup_parser()
 
-        print("Video downloaded successfully.")
-    except Exception as e:
-        print("An error occurred while downloading the video:", str(e))
+    def setup_parser(self):
+        self.parser = argparse.ArgumentParser(description="YouTube Video Downloader")
+        self.parser.add_argument('-u', '--url', help="URL of the YouTube video to download")
+        self.parser.add_argument('-f', '--file', help="File containing a list of YouTube video URLs to download")
+
+    def download_video(self, video_url):
+        try:
+            youtube = pytube.YouTube(video_url)
+            video_stream = youtube.streams.get_highest_resolution()
+            video_stream.download()
+
+            original_filename = video_stream.default_filename
+            print(f"Downloading {original_filename}")
+            new_filename = original_filename.replace(" ", "-")
+            os.rename(original_filename, new_filename)
+
+            print("Video downloaded successfully.")
+        except Exception as e:
+            print("An error occurred while downloading the video:", str(e))
+
+    def download_videos_from_file(self, file_path):
+        if not os.path.exists(file_path):
+            print(f"The file {file_path} does not exist.")
+            return
+
+        with open(file_path, 'r') as file:
+            urls = file.readlines()
+
+        for url in urls:
+            url = url.strip()
+            if url:
+                print(f"Processing URL: {url}")
+                self.download_video(url)
+
+    def run(self):
+        args = self.parser.parse_args()
+
+        if args.url:
+            self.download_video(args.url)
+        elif args.file:
+            self.download_videos_from_file(args.file)
+        else:
+            print("Please provide a URL with -u or a file with -f.")
 
 if __name__ == "__main__":
-    video_url = sys.argv[1] if len(sys.argv) > 1 else None
-
-    if video_url:
-        download_video(video_url)
-    else:
-        print("Please provide a valid video URL as a command line argument.")
+    downloader = YouTubeDownloader()
+    downloader.run()
 
 """
+pip install pytube
 to convert to mp3
 ffmpeg -i filename.mp4 filename.mp3
-```
+"""
